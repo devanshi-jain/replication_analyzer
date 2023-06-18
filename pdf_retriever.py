6b42a98462d7602aa3ae7a98ed8c39e243266c5e
Manan Ronngta <mananr@berkeley.edu>
2023-06-17 19:45:01 -0700

    pdf retriever

--- pdf_retriever.py
+++ pdf_retriever.py
@@ -8,17 +8,14 @@ data= pd.read_csv("Ju, Yiguang.csv")
 no_of_rows = data.shape[0]
 
 file_path = "paperdata.json"
-
 for i in range(0, no_of_rows): 
 
     try:
         doi = data.iloc[i, 12]
         title = data.iloc[i, 2]
-        paper = "https://doi.org/" + DOI
-        
+        paper = "https://doi.org/" + doi
         out = os.getcwd() + "/downloadedPapers" + doi
         paper_type = "doi"
-
         proxies = {
             'http': 'socks5://127.0.0.1:7890'
         }
