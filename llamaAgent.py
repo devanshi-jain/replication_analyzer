8591739b876a227eafa09af0dfbbea26bc2e4366
Devanshi Jain <djain@ucsd.edu>
2023-06-18 11:22:09 -0700

    commit updates

--- llamaAgent.py
+++ llamaAgent.py
@@ -5,12 +5,15 @@ from llama_index import (
     LLMPredictor,
     ServiceContext
 )
+
 from langchain.llms.openai import OpenAIChat
 from llama_index.indices.composability import ComposableGraph
 from llama_index.indices.query.query_transform.base import DecomposeQueryTransform
 from llama_index.query_engine.transform_query_engine import TransformQueryEngine
 import openai
 import os
+
+from langchain.chat_models import ChatOpenAI
 
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) #reads local .env file
@@ -20,8 +23,6 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Set the API key for the OpenAI API client
 openai.api_key = openai_api_key
-
-print("OpenAI API key:", openai.api_key)
 
 def llamaAgent(pdfAPath, pdfBPath):
     pdfs = {}
--- promptAgent.py
+++ promptAgent.py
@@ -34,7 +34,7 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 # Set the API key for the OpenAI API client
 openai.api_key = openai_api_key
 
-print("OpenAI API key:", openai.api_key)
+# print("OpenAI API key:", openai.api_key)
 
 # function uses the OpenAI API client to generate text based on a prompt using 
 def get_completion(prompt, model="gpt-3.5-turbo-16k"):
@@ -82,105 +82,110 @@ def calculate_internal_score(citing_paper):
     return score
 
 
-
 def check_reproduction(pdfA_path, pdfB_path, indicator, abstract = None): # indicator = True for paper and False for abstract
-    output_string = StringIO()
-    # Extract the citations from PDF1
-    # citation_pos_pdfA = parse_pdf_citations(pdfB_path)
-    # print("Citations from PDFB:", citation_pos_pdfA, "\n")
-    # with open(pdfA_path, 'rb') as pdfA_file:
-    #         parser = PDFParser(pdfA_file)
-    #         document = PDFDocument(parser)
-    #         parser.set_document(document)
-    #         metadata = document.info[0]
-    #         title = metadata.get()
-    # find the index within these citations that match the title of pdfA
-    # for i in range(len(citation_pos_pdfA)):
-    #     if citation_pos_pdfA[i] == title:
-    #         citation_pos_pdfA = i
-    #         break
-
     if indicator == True:
-        # Extract the content of PDF2 for citation analysis
-        # with open(pdfB_path, 'rb') as pdfB_file:
-        #     parser = PDFParser(pdfB_file)
-        #     document = PDFDocument(parser)
-        #     parser.set_document(document)
-        #     metadata = document.info[0]
-        #     title = metadata.get('Title', '')
-        #     author = metadata.get('Author', '')
-        #     rsrcmgr = PDFResourceManager()
-        #     device = PDFPageAggregator(rsrcmgr, laparams=LAParams())
-        #     interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-        #     citations_found = []
-        #     for page in PDFPage.create_pages(document):
-        #         interpreter.process_page(page)
-        #         content = output_string.getvalue()
-        #         output_string.seek(0)
-        #         output_string.truncate(0)
-        #         interpreter.process_page(page)
-        #         layout = device.get_result()
-        #         for element in layout:
-        #             if isinstance(element, LTTextBox) or isinstance(element, LTTextLine):
-        #                 text = element.get_text().strip()
-        #                 # Check if the text contains any citations from PDF1
-        #                 # for citation in citation_pos_pdfA:
-        #                 #     # if f"[{citation}]" in text: 
-        #                 citations_found.append(text)
-            output_string = StringIO()
-            with open(pdfB_path, 'rb') as pdfB_file:
+        output_string = StringIO()
+        with open(pdfB_path, 'rb') as pdfB_file:
+            with pdfB_file as in_pdfB_file:
                 resource_manager = PDFResourceManager()
                 device = TextConverter(resource_manager, output_string, laparams=LAParams())
                 interpreter = PDFPageInterpreter(resource_manager, device)
-                parser = PDFParser(pdfB_file)
+                parser = PDFParser(in_pdfB_file)
                 document = PDFDocument(parser)
+                total_pages = len(list(PDFPage.get_pages(in_pdfB_file, maxpages=0, caching=True, check_extractable=True)))
                 metadata = document.info[0]
                 title = metadata.get('Title', '')
-                author = metadata.get('Author', '')
-                for page_number, page in enumerate(PDFPage.get_pages(pdfB_file, maxpages=0, caching=True, check_extractable=True), start=1):
+                pdfB_book1 = []
+                pdfB_book2 = []
+                half_point = total_pages // 2
+                for page_number, page in enumerate(PDFPage.get_pages(in_pdfB_file, maxpages=0, caching=True, check_extractable=True), start=1):
                     interpreter.process_page(page)
                     pdfB_page = output_string.getvalue()
                     output_string.seek(0)
                     output_string.truncate(0)
-                    # Send prompt to OpenAI API to check if PDF2 reproduces the results of PDF1
-                    prompt = f"""
-                    Reproducibility is a fundamental aspect of scientific research, ensuring that the results, findings, and methodologies
-                    presented in a paper can be independently validated and verified. You need to act like a Research Engineer in the 
-                    field of mechanical and aerospace engineering. Part of your role involves assessing the reproducibility of academic
-                    papers and assess their effectiveness in addressing specific engineering challenges. 
+                    if(page_number <= half_point):
+                        pdfB_book1.append(pdfB_page)
+                    else:
+                        pdfB_book2.append(pdfB_page)
+                # print("\nContent: ", pdfB_book, "\n")
+                # print("Page Number:", page_number, "\nContent: ", pdfB_page, "\n")
+                # Send prompt to OpenAI API to check if PDF2 reproduces the result`s of PDF1
+                prompt1 = f"""
+                        Reproducibility is a fundamental aspect of scientific research, ensuring that the results, findings, and methodologies
+                        presented in a paper can be independently validated and verified. You need to act like a Research Engineer in the 
+                        field of mechanical and aerospace engineering. Part of your role involves assessing the reproducibility of academic
+                        papers and assess their effectiveness in addressing specific engineering challenges. 
 
-                    You are given two PDFs, PDFA and PDFB. PDFA is the original paper, which is delimited by triple backticks. PDFB is a
-                    paper that cites PDFA. You are given the relevant pages within PDFB that cite and utilize PDFA i.e. PDFB citations,
-                    which is delimited by double backticks. Your task is to check if PDFB reproduces the results of PDFA.
+                        You are given two PDFs, PDFA and pages in PDFB. PDFA is the original paper, which is delimited by triple backticks. PDFB 
+                        is a paper that cites PDFA, and is given page by page which is delimited by double backticks. 
+                        Iteratively, your task is to check if PDFB reproduces the results of PDFA.
 
-                    To analyze the reproducibility of PDFA based on the citations and content in PDFB, follow these directions:
-                    
-                    1. Examine PDFB citations. Look for indications of whether PDFB attempted to reproduce the results 
-                    of PDFA or if it simply cited PDFA as a reference without attempting reproduction.
-                    2. If PDFB states or implies that it reproduced PDFA's results, carefully scrutinize the corresponding sections or 
-                    code snippets in PDFB. Compare the reported results in PDFA with those in PDFB to verify if they align.
-                    3. Note any discrepancies or differences in the reproduced results between PDFA and PDFB. If PDFB successfully 
-                    reproduces PDFA's results, clearly state that the reproduction was successful. On the other hand, if PDFB produces 
-                    results that directly contradict or deviate significantly from PDFA's results, explicitly mention the discrepancies
-                    and highlight that PDFB's results do not align with PDFA's findings.
-                    4. Provide a detailed analysis of the similarities or differences between PDFA and PDFB, specifically in terms of the
-                    reproduced results. Explain any modifications, variations, or potential reasons for the discrepancies, if applicable.
+                        To analyze the reproducibility of PDFA based on the citations and content in PDFB, follow these directions:
+                        
+                        1. Examine PDFB citations. Look for indications of whether PDFB attempted to reproduce the results 
+                           of PDFA or if it simply cited PDFA as a reference without attempting reproduction.
+                        2. If PDFB states or implies that it reproduced PDFA's results, carefully scrutinize the corresponding sections or 
+                           code snippets in PDFB. Compare the reported results in PDFA with those in PDFB to verify if they align.
+                        3. Note any discrepancies or differences in the reproduced results between PDFA and PDFB. If PDFB successfully 
+                           reproduces PDFA's results, clearly state that the reproduction was successful. On the other hand, if PDFB produces 
+                           results that directly contradict or deviate significantly from PDFA's results, explicitly mention the discrepancies
+                           and highlight that PDFB's results do not align with PDFA's findings.
+                        4. Provide a detailed analysis of the similarities or differences between PDFA and PDFB, specifically in terms of the
+                           reproduced results. Explain any modifications, variations, or potential reasons for the discrepancies, if applicable.
 
-                    Based on this assement, give it a score between 0 and 10, where 0 indicates that PDFB does not reproduce PDFA's results
-                    at all, and 10 indicates that PDFB reproduces PDFA's results perfectly.
+                        Based on this assement, give it a score between 0 and 10, where 0 indicates that PDFB does not reproduce PDFA's results
+                        at all, and 10 indicates that PDFB reproduces PDFA's results perfectly. 
+                PDFA: '''{pdfA_path}'''
+                PDFB page: ''{pdfB_book1}''
+                """
+                response = get_completion(prompt1)
+                # print("Response from OpenAI API:")
+                print("\nResponse ", response, ":",)
+                device.close()
+                response = output_string.getvalue()
+                # output_string.close()
 
-                    PDFA: '''{pdfA_path}'''
-                    PDFB citations: ''{pdfB_page}''
-                    """
-                    response = get_completion(prompt)
-                    print("Response from OpenAI API:")
-                    # print(response)
-                    device.close()
-                    response = output_string.getvalue()
-                    output_string.close()
-                    return response
 
+                prompt2 = f"""
+                        Reproducibility is a fundamental aspect of scientific research, ensuring that the results, findings, and methodologies
+                        presented in a paper can be independently validated and verified. You need to act like a Research Engineer in the 
+                        field of mechanical and aerospace engineering. Part of your role involves assessing the reproducibility of academic
+                        papers and assess their effectiveness in addressing specific engineering challenges. 
+
+                        You are given two PDFs, PDFA and pages in PDFB. PDFA is the original paper, which is delimited by triple backticks. PDFB 
+                        is a paper that cites PDFA, and is given page by page which is delimited by double backticks. 
+                        Iteratively, your task is to check if PDFB reproduces the results of PDFA.
+
+                        To analyze the reproducibility of PDFA based on the citations and content in PDFB, follow these directions:
+                        
+                        1. Examine PDFB citations. Look for indications of whether PDFB attempted to reproduce the results 
+                           of PDFA or if it simply cited PDFA as a reference without attempting reproduction.
+                        2. If PDFB states or implies that it reproduced PDFA's results, carefully scrutinize the corresponding sections or 
+                           code snippets in PDFB. Compare the reported results in PDFA with those in PDFB to verify if they align.
+                        3. Note any discrepancies or differences in the reproduced results between PDFA and PDFB. If PDFB successfully 
+                           reproduces PDFA's results, clearly state that the reproduction was successful. On the other hand, if PDFB produces 
+                           results that directly contradict or deviate significantly from PDFA's results, explicitly mention the discrepancies
+                           and highlight that PDFB's results do not align with PDFA's findings.
+                        4. Provide a detailed analysis of the similarities or differences between PDFA and PDFB, specifically in terms of the
+                           reproduced results. Explain any modifications, variations, or potential reasons for the discrepancies, if applicable.
+
+                        Based on this assement, give it a score between 0 and 10, where 0 indicates that PDFB does not reproduce PDFA's results
+                        at all, and 10 indicates that PDFB reproduces PDFA's results perfectly. 
+                PDFA: '''{pdfA_path}'''
+                PDFB page: ''{pdfB_book2}''
+                """
+                response = get_completion(prompt2)
+                # print("Response from OpenAI API:")
+                print("\nResponse ", response, ":",)
+                device.close()
+                response = output_string.getvalue()
+                output_string.close()
+                    # return response
+                    # You are given two PDFs, PDFA and PDFB. PDFA is the original paper, which is delimited by triple backticks. PDFB is a
+                    # paper that cites PDFA. You are given the relevant pages within PDFB that cite and utilize PDFA i.e. PDFB citations,
+                    # which is delimited by double backticks. Your task is to check if PDFB reproduces the results of PDFA.
+                    # Based on this assement, give it a score between 0 and 10, where 0 indicates that PDFB does not reproduce PDFA's results
+                    # at all, and 10 indicates that PDFB reproduces PDFA's results perfectly.
 
             # print("Citations found:", citations_found, "\n")
 
