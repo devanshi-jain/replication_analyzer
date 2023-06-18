ffcf5db2b1427b2410480152746bd1edf1708ba6
Jet Situ <situjet@gmail.com>
2023-06-17 23:07:24 -0700

    checking paperlite

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import opencitingpy
-from paper_repository import Paper
+from paper_repository import Paper, PaperLite
 
 def createPaperFromDoi(doi):
     client = opencitingpy.client.Client()
@@ -10,6 +10,14 @@ def createPaperFromDoi(doi):
         return -1
     metadata = metadata[0]
     return Paper(title = metadata.title, doi = doi, cited_by = cited_by, sources = sources, publication_date = metadata.year)
+
+def createPaperLiteFromDoi(doi):
+    client = opencitingpy.client.Client()
+    metadata = client.get_metadata(doi)
+    if metadata == []:
+        return -1
+    metadata = metadata[0]
+    return PaperLite(title = metadata.title, doi = doi, publication_date = metadata.year)
 
 #outputs a return code: 0 if retrieved successfully, -1 if not retrieved (will be used to determine whether or not to use abstract)
 def retrievePaperFromDrive(title):
